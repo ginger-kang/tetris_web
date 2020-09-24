468723534e183e2b6550c86798d012141d8e5da7
ginger-kang <kdhoon07@gmail.com>
2020-09-25 06:59:00 +0900

    :nail_care: add style components

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+
+import { TETROMINOS } from "../tetrominos";
+
+interface cProps {
+  type: any;
+  color: string;
+}
+
+const StyledCell = styled.div<cProps>`
+  width: auto;
+  background: rgba(${(props) => props.color}, 0.8);
+  border: ${(props) => (props.type === 0 ? "0px solid" : "4px solid")};
+  border-bottom-color: rgba(${(props) => props.color}, 0.1);
+  border-right-color: rgba(${(props) => props.color}, 1);
+  border-top-color: rgba(${(props) => props.color}, 1);
+  border-left-color: rgba(${(props) => props.color}, 0.3);
+`;
 
 function Cell({ type }) {
-  return <div>cell</div>;
+  return <StyledCell type={type} color={TETROMINOS[type].color}></StyledCell>;
 }
 
 export default Cell;
--- src/components/Tetris.tsx
+++ src/components/Tetris.tsx
@@ -1,22 +1,48 @@
 import React from "react";
+import styled from "styled-components";
+
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
 import { createStage } from "../gameHelpers";
 
+const StyledTetrisWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: #000;
+  background-size: cover;
+  overflow: hidden;
+`;
+
+const StyledTetris = styled.div`
+  display: flex;
+  align-items: flex-start;
+  padding: 40px;
+  margin: 0 auto;
+  max-width: 900px;
+  aside {
+    width: 100%;
+    max-width: 200px;
+    display: block;
+    padding: 0 20px;
+  }
+`;
+
 const Tetris = () => {
   return (
-    <div>
-      <Stage />
-      <aside>
-        <div>
-          <Display text="Score" />
-          <Display text="Rows" />
-          <Display text="Level" />
-        </div>
-        <StartButton />
-      </aside>
-    </div>
+    <StyledTetrisWrapper>
+      <StyledTetris>
+        <Stage stage={createStage()} />
+        <aside>
+          <div>
+            <Display text="Score" />
+            <Display text="Rows" />
+            <Display text="Level" />
+          </div>
+          <StartButton />
+        </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
   );
 };
 
