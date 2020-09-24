20a77ab9f9fa972d2f3bef7d1bca5c2b6504d008
ginger-kang <kdhoon07@gmail.com>
2020-09-25 06:44:45 +0900

    :sparkles: add tetrominos and set noImplicityAny

--- src/components/Tetris.tsx
+++ src/components/Tetris.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
+import { createStage } from "../gameHelpers";
 
 const Tetris = () => {
   return (
